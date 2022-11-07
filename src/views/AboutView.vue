09a1766a74df555f68816d9599a3cb0c0428e6d4
Guapura89 <betocrack1228@gmail.com>
2022-11-06 18:18:33 -0600

    Initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="progress w-50 mx-auto">
+      <div
+        class="progress-bar w-75"
+        role="progressbar"
+        aria-valuenow="75"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      ></div>
+    </div>
   </div>
 </template>
